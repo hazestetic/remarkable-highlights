55ec5f13647e2b41cf70eaa31d7e93a29b470f7d
Marcin Tyszkiewicz <t.marcinn@gmail.com>
2021-12-27 15:43:19 +0100

    DeepL translator + diskcache

---
+++ translator.py
@@ -0,0 +1,32 @@
+import os
+import deepl
+from diskcache import Cache
+from pathlib import Path
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)s : %(message)s"
+)
+
+def translate(text: str) -> str:
+    """
+    Tłumaczy podany tekst na język polski, 
+    korzystając z cache'a gdy to mozliwe.
+    W przeciwnym wypadku tłumeczenie jest pozyskiwane z DeepL API.
+    """
+    CACHE_DIR = Path(__file__).parent.joinpath("data/cache")
+    cache = Cache(CACHE_DIR)
+    if text in cache:
+        logging.info("Translation loaded from cache.")
+        return cache[text]
+    else:
+        translator = deepl.Translator(os.getenv("DEEPL_AUTH_KEY"))
+        result = translator.translate_text(text, target_lang="PL")
+        cache[text] = result.text
+        logging.info("Translation fetched from DeepL API.")
+        return result.text
+
+if __name__ == "__main__":
+    res = translate("This is a test text")
+    print(res)
